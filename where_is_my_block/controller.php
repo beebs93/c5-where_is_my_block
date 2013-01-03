7a80fa8d1851020d0e6be2e7f7ac1d08f3535016
Brad Beebe <beebs93@gmail.com>
2013-01-02 22:06:17 -0800

    Fixes #24

--- where_is_my_block/controller.php
+++ where_is_my_block/controller.php
@@ -3,8 +3,8 @@ defined('C5_EXECUTE') or die(_('Access Denied.'));
 
 class WhereIsMyBlockPackage extends Package{
 	protected $pkgHandle = 'where_is_my_block';
-	protected $appVersionRequired = '5.5.2.1';
-	protected $pkgVersion = '0.9.0.8';
+	protected $appVersionRequired = '5.6.0.2';
+	protected $pkgVersion = '0.9.0.9';
 	
 	
 	/**
